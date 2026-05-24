567fbe09ea5e281e6eb8fea2967631502d3f3314
Angel Eliam Contreras Santiago <L22350395@tuxtepec.tecnm.mx>
2026-05-24 15:41:03 -0600

    Textos, Row, Columnas, SingleChildScrollView

--- lib/second_file.dart
+++ lib/second_file.dart
@@ -8,34 +8,47 @@ class SecondClass extends StatelessWidget{
   Widget build(BuildContext context) {
 
     return Material(
-          color: Colors.deepPurpleAccent,
-          child: Container(
+          //color: Colors.deepPurpleAccent,
+          child: SingleChildScrollView(
+            child: Column(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: <Widget>[
+
+
+              Icon(Icons.ac_unit),
+            Container(
             height: 500.0,
-            width: 300.0, 
-            alignment: Alignment(0,0),
-            decoration: BoxDecoration(
-              gradient: RadialGradient(
-                colors:[Colors.green, Colors.blue, Colors.orange,Colors.pink],
-                stops: [0.2,0.5,0.7,1],
-                center: Alignment(0.1, 0.3),
-                focal: Alignment(-0.1, 0.6)
-                  )
-            ),
-            //color: lightPurpleColor,
-            child: GestureDetector(
-              onTap: (){
-                ScaffoldMessenger.of(context).showSnackBar(
-                  SnackBar(
-                    content: Text('It was pressed'),
-                    duration: Duration(seconds: 2),
-                ),
-                );;
-              },
-              child: Text(generateNumbers(), textDirection: TextDirection.ltr,),
-            )
-            ,
-        )
-      );
+            width: 300.0,
+            // alignment: Alignment(0,0),
+            // decoration: BoxDecoration(
+            //   gradient: RadialGradient(
+            //     colors:[Colors.green, Colors.blue, Colors.orange,Colors.pink],
+            //     stops: [0.2,0.5,0.7,1],
+            //     center: Alignment(0.1, 0.3),
+            //     focal: Alignment(-0.1, 0.6)
+            //       )
+            // ),
+            color: lightPurpleColor,
+            // child: GestureDetector(
+            //   onTap: (){
+            //     ScaffoldMessenger.of(context).showSnackBar(
+            //       SnackBar(
+            //         content: Text('It was pressed'),
+            //         duration: Duration(seconds: 2),
+            //     ),
+            //     );
+            //   },
+            //   child: Text(generateNumbers(), textDirection: TextDirection.ltr, style: TextStyle(color: Colors.white, fontSize: 15.0, fontWeight: FontWeight.bold),),
+            // ),
+        ),
+      Container(
+        height: 500.0,
+        width: 100.0,
+        color: Colors.green,
+      )
+      ] 
+          )));
+      
   }
   
 
